2b50db14ad2b49be20d8f08dac1d75b316c08644
AnnaTeachMeSkills <60443531+AnnaTeachMeSkills@users.noreply.github.com>
2020-03-27 15:09:47 +0300

    Add files via upload

--- homeworkles19/js/script.js
+++ homeworkles19/js/script.js
@@ -1,18 +1,17 @@
-let mass = [5,3,10,7,1,10];
+let mass = [5,3,10,7,1,10,'Hello',"100"];
 
 
 Array.prototype.sum = function () {
     let sum = 0;
 
     for(let i = 0; i < this.length; i++){
-        if (typeof this[i] == 'number') {
-            sum += this[i];
-           
+        if (!isNaN(Number(this[i]))) {
+            sum += Number(this[i]);
         }
+
     }
     return sum;
 }
-
 
 console.log(mass.sum());
 
